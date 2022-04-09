4cd819186fd6d10cfe9e6ca5d4c8f2ff18909262
thatwhocame <66080483+thatwhocame@users.noreply.github.com>
2022-04-09 09:35:57 +0300

    added 2 commands to tltgram bot

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import telebot
-from valuables import telegramApi
+import logging
+from valuables import *
+
 bot = telebot.TeleBot(telegramApi)
 
-#todo
 
-bot.polling(none_stop=True, interval=0)
+@bot.message_handler(commands=['start'])
+def start_msg(message):
+    bot.send_message(message.from_user.id, start_mess)
+
+
+@bot.message_handler(commands=['help'])
+def help_msg(message):
+    bot.send_message(message.from_user.id, help_mess)
+
+
+def main(use_logging, level_name):
+    if use_logging:
+        telebot.logger.setLevel(logging.getLevelName(level_name))
+    bot.polling(none_stop=True, interval=.5)
+
+bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    main(True, 'DEBUG')
--- valuables.py
+++ valuables.py
@@ -1,2 +1,5 @@
-telegramApi = ''
+telegramApi = '5230859161:AAH6RfHAPgJMm4-J8phe-uAUAZxLqIVU-rw'
 coincapApi = ''
+
+start_mess = 'Привет! Я могу отправлять тебе уведомления о курсе криптовалют столько раз в день, сколько тебе нужно!'
+help_mess  = 'Напиши /start, чтобы начать.'
